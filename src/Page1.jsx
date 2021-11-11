bde17d6e8aac6aa607a95952fa401ce31b691d6c
yota-g <64677279+yota-g@users.noreply.github.com>
2021-11-11 23:56:39 +0900

    leran about route without Route

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,12 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  // console.log(arr);
+  //detailAに遷移したときにこの配列をそのまま持っていくという実装をする。
+  const history = useHistory();
+  const onClickDetailA = () => history.push("/page1/detailA");
+  // pushの中にlinkのtoの部分を記載する。linkと同じように画面遷移することができる。
   return (
     <div>
       <h1>Page1ページです。</h1>
-      <Link to="/page1/detailA">DetailA</Link>
+      {/* <Link to="/page1/detailA">DetailA</Link> */}
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
+      <br />
+      {/* Link以外のページ遷移の仕方 */}
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
     // nest遷移されたものを変更するときはApp.jsのRoute部分を変更しないといけない。
   );
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,18 @@
+import { useLocation, useHistory } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  // const location = useLocation();
+  // console.log(location);
+  const { state } = useLocation();
+  console.log(state);
+  // 配列を取得することができる。
+  const history = useHistory();
+  const onClickBack = () => history.goBack();
+  //goBackで前のページに戻ることができる。
   return (
     <div>
       <h1>Page1DetailAページです。</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
